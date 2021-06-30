f257267543118736cfb92f8381131eed6d1ff9b8
Mmalikov1337 <grelexis1337@inbox.ru>
2021-06-30 20:52:35 +0600

    add validation for /register endpoint

---
+++ src/middlewares/validationHandler.ts
@@ -0,0 +1,14 @@
+import { NextFunction, Request, Response } from "express";
+import { validationResult } from "express-validator";
+import ClientError from "../errors/ApiError";
+
+const validationHandler = (req: Request, res: Response, next: NextFunction) => {
+	const errors = validationResult(req);
+	
+	if (!errors.isEmpty()) {
+		return next(ClientError.badRequest("Validation error.", errors.array()));
+	}
+	next();
+};
+
+export default validationHandler;
--- src/routers/authRouter.ts
+++ src/routers/authRouter.ts
@@ -1,10 +1,17 @@
 import express from "express";
+import { body } from "express-validator";
 import UserController from "../controllers/UserController";
-
+import validationHandler from "../middlewares/validationHandler";
 
 const auth = express.Router();
 
-auth.post("/register", UserController.register);
+auth.post(
+	"/register",
+	body("email").isEmail(),
+	body("password").isLength({ min: 3, max: 32 }),
+	validationHandler,
+	UserController.register
+);
 
 auth.post("/login", UserController.login);
 
